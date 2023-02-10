b1175596b4548358bd34e22a488c72f46f713eee
singhtushar689 <singhtushar689@gmail.com>
2023-02-10 23:06:13 +0530

    [Tushar] add: Find Negative in the array

---
+++ ArrayBasedProblem/FIndNegativeElements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasedProblems
+{
+    public class FIndNegativeElements
+    {
+        public void FindNegativeElements()
+        {
+            int[] array = { 23, 42, -4, 8, -54, 39, 98 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
