87666eb3cf19719391f0df86c0b9b60b4408cdfd
singhtushar689 <singhtushar689@gmail.com>
2023-02-10 23:42:15 +0530

    [Tushar] add: Sum of Elements

---
+++ ArrayProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any Option \n 1.ArrayLength \n 2 .FindMaxMinInElements " +
+                "\n 3 . Showing Negative Elements \n 4 .SumOfAllElements");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                   FindLengthOfArray arr = new FindLengthOfArray(); 
+                    arr.ArrayLength();
+                    break;
+                case 2:
+                    FindMaxMin max = new FindMaxMin();
+                    max.MaxMin();
+                    break;
+                case 3:
+                    NegativeElements negative = new NegativeElements();
+                    negative.Negative();
+                    break;
+                case 4:
+                    SumOfElements sum = new SumOfElements();
+                    sum.Sum();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
+
---
+++ ArrayProblem/SumOfElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblem
+{
+    public class SumOfElements
+    {
+        public void Sum()
+        {
+            int[] array = { 23, 57, -46, 73, -15, 98, 68 };
+            int sum = 0;
+            foreach(int elements in array)
+            {
+                Console.WriteLine("The Elements are " + elements);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine("Sum of all Elements of Array is " + sum);
+        }
+    }
+}
